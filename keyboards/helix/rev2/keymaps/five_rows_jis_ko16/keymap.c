64c1b9fde516dc654a2de2219b65171a955cef97
Koichiro Ohba <koichiro@meadowy.org>
2018-09-03 23:38:16 +0900

    abolish inline functions

--- keyboards/helix/rev2/keymaps/five_rows_jis_ko16/keymap.c
+++ keyboards/helix/rev2/keymaps/five_rows_jis_ko16/keymap.c
@@ -5,6 +5,7 @@
 #include "lufa.h"
 #include "split_util.h"
 #endif
+#define SSD1306OLED
 #ifdef SSD1306OLED
   #include "ssd1306.h"
 #endif
@@ -79,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,   \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_COLN, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,  \
-    KC_ZHTG, KC_LALT, KC_LGUI, ML_ADJ,  ML_LOW,  KC_SPC,  KC_BSPC, KC_ENT,  KC_SPC,  ML_RAI,  KC_APP,  KC_LEFT, KC_DOWN, KC_RGHT  \
+    JP_ZHTG, KC_LALT, KC_LGUI, ML_ADJ,  ML_LOW,  KC_SPC,  KC_BSPC, KC_ENT,  KC_SPC,  ML_RAI,  KC_APP,  KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
 
   /* Qwerty JIS Exchange L and R
@@ -223,7 +224,7 @@ const char code_to_name[60] = {
     'R', 'E', 'B', 'T', ' ', '-', ' ', '@', ' ', ' ',
     ' ', ';', ':', ' ', ',', '.', '/', ' ', ' ', ' '};
 
-inline void set_keylog(uint16_t keycode, keyrecord_t *record)
+void set_keylog(uint16_t keycode, keyrecord_t *record)
 {
   char name = ' ';
   uint8_t leds = host_keyboard_leds();
@@ -344,7 +345,7 @@ void matrix_scan_user(void) {
   iota_gfx_task();  // this is what updates the display continuously
 }
 
-inline void matrix_update(struct CharacterMatrix *dest,
+void matrix_update(struct CharacterMatrix *dest,
                           const struct CharacterMatrix *source) {
   if (memcmp(dest->display, source->display, sizeof(dest->display))) {
     memcpy(dest->display, source->display, sizeof(dest->display));
@@ -367,13 +368,13 @@ const char helix_logo[]={
   0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
   0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,
   0};
-inline void render_logo(struct CharacterMatrix *matrix) {
+void render_logo(struct CharacterMatrix *matrix) {
 
   matrix_write(matrix, helix_logo);
 }
 
 const char mac_win_logo[][2][3]={{{0x95,0x96,0},{0xb5,0xb6,0}},{{0x97,0x98,0},{0xb7,0xb8,0}}};
-inline void render_status(struct CharacterMatrix *matrix) {
+void render_status(struct CharacterMatrix *matrix) {
 
   char buf[20];
   // Render to mode icon
