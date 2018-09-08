f353972b909349b2ae7375e8985a2e3dfc699c92
Koichiro Ohba <koichiro@meadowy.org>
2018-09-08 17:11:01 +0900

    fix alt key

--- keyboards/helix/rev2/keymaps/five_rows_jis_ko16/keymap.c
+++ keyboards/helix/rev2/keymaps/five_rows_jis_ko16/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   B  |   N  |   M  |  ,<  |  .>  |  Up  |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Kanji| GUI  | Alt  |Adjust|Lower |Space |Bksp  |Enter |Space |Raise | APP  | Left | Down |Right |
+   * | Kanji|Adjust|Lower | GUI  | Alt  |Space |Bksp  |Enter |Space |Raise | APP  | Left | Down |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_BASE] = LAYOUT( \
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,   \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_COLN, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,  \
-    JP_ZHTG, KC_LALT, KC_LGUI, ML_ADJ,  ML_LOW,  KC_SPC,  KC_BSPC, KC_ENT,  KC_SPC,  ML_RAI,  KC_APP,  KC_LEFT, KC_DOWN, KC_RGHT  \
+    JP_ZHTG, ML_ADJ,  ML_LOW,  KC_LALT, KC_LGUI, KC_SPC,  KC_BSPC, KC_ENT,  KC_SPC,  ML_RAI,  KC_APP,  KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
 
   /* Qwerty JIS Exchange L and R
